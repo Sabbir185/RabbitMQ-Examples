08c3e8bdd3c294939c73907de01e05efcb88f300
sabbir185 <sabbir.cse@yahoo.com>
2026-04-17 12:59:23 +0600

    producer publish msg with timeout context

--- 01. simple RMQ/producer/producer.go
+++ 01. simple RMQ/producer/producer.go
@@ -5,8 +5,12 @@ package main
 // nameless exchange is used, so the routing key is the same as the queue name.
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"time"
 
 	"github.com/rabbitmq/amqp091-go"
 )
@@ -39,9 +43,33 @@ func main() {
 		log.Fatalln("Queue declaration error!", err)
 	}
 
-	err = ch.Publish("", q.Name, false, false, amqp091.Publishing{
+	// simple publisher that sends a message to the queue
+	/*
+		err = ch.Publish("", q.Name, false, false, amqp091.Publishing{
+			ContentType: "text/plain",
+			Body: []byte("Hello, RabbitMQ"),
+		})
+	*/
+
+
+	// stopping docker container to create a timeout scenario for the publish operation
+	cmd := exec.Command("docker", "stop", "rabbitmq")
+	err = cmd.Run()
+	if err != nil {
+		log.Fatalln("Failed to stop RabbitMQ container!", err)
+	}
+	time.Sleep(2 * time.Second) // wait for the container to stop
+
+
+	// publish msg with context, which allows us to set a timeout for the publish operation
+	cxt, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	fmt.Println("Attempting to publish a message to the queue...")
+
+	err = ch.PublishWithContext(cxt, "", q.Name, false, false, amqp091.Publishing{
 		ContentType: "text/plain",
-		Body: []byte("Hello, RabbitMQ"),
+		Body: []byte("Hello, RabbitMQ !"),
 	})
 	if err != nil {
 		log.Fatalln("Failed to publish a message!", err)
